b70a6cc012fa91580fe0183bbac4c039d0e658a0
B-kumaraswamy <kumaraswamy491@gmail.com>
2025-06-03 17:39:56 +0530

    Refactor CORS configuration to use environment variable for frontend URL

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -8,7 +9,10 @@ app = FastAPI(title="Pipeline Builder API", version="1.0.0")
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+     allow_origins=[
+        os.environ.get("VITE_REACT_APP_FRONTEND_URL", "http://localhost:3000"),
+        "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
